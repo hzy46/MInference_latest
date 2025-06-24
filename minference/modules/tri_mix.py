e189ff857031b229623515cf14d79e58d653eab7
Zhiyuan He <zhiyuhe@microsoft.com>
2025-06-24 17:35:06 +0800

    fix

--- minference/modules/tri_mix.py
+++ minference/modules/tri_mix.py
@@ -51,7 +51,7 @@ def minference_mix_forward(q, k, v, prefill_kwargs):
     if layer_idx == 31:
         for i in range(32):
             torch.cuda.synchronize()
-            start_event, end_event = g["timer"][i]:
+            start_event, end_event = g["timer"][i]
             elapsed_time_ms = start_event.elapsed_time(end_event)
             print("Layer {} Cost {:.3f} second.".format(i, elapsed_time_ms / 1000.))
     return result
