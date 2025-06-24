248bdede4a01a42c98a4174a3f91320b35497e3c
Zhiyuan He <zhiyuhe@microsoft.com>
2025-06-24 18:15:41 +0800

    fix

--- tri_mix/speed_test.py
+++ tri_mix/speed_test.py
@@ -115,18 +115,18 @@ def quick_get_random_kv_samples(model_name, tokenizer, gold_index, n_kv_num=10, 
 
 def main(model_name="meta-llama/Llama-3.1-8B-Instruct", method="dense", starting_layer=None):
     seq_len_list = [
-        # 4000, 
-        # 8000, 
-        # 16000, 
-        # 32000,
-        # 64000, 
+        4000, 
+        8000, 
+        16000, 
+        32000,
+        64000, 
         128000
         # 64000,
         # 72000,
         # 80000,
         # 96000,
     ]
-    n_times = 1
+    n_times = 3
     if starting_layer is None:
         if model_name == "meta-llama/Llama-3.1-8B-Instruct":
             starting_layer = 16
