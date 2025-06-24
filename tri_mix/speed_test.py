e18bec1c35e3128d7596825835a5bfdfac1f20e5
Zhiyuan He <zhiyuhe@microsoft.com>
2025-06-24 18:07:51 +0800

    fix

--- tri_mix/speed_test.py
+++ tri_mix/speed_test.py
@@ -146,13 +146,12 @@ def main(model_name="meta-llama/Llama-3.1-8B-Instruct", method="dense", starting
         )
     elif method == "flexprefill":
         kwargs = dict(
-            attn_type="flexprefill_mix",
-            attn_kwargs={"last_n": 128, "starting_layer": 10000, "n_local": 512, "n_init": 8},
+            attn_type="flexprefill",
+            attn_kwargs={"gamma": 2},
         )
     elif method == "minference":
         kwargs = dict(
-            attn_type="minference_mix",
-            attn_kwargs={"last_n": 128, "starting_layer": 10000, "n_local": 512, "n_init": 8},
+            attn_type="minference",
         )
     elif method == "minference_mix":
         kwargs = dict(
