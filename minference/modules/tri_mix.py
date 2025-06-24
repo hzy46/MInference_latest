89040d8dafd96ac08ba9520ba87daf095ca95d6c
Zhiyuan He <zhiyuhe@microsoft.com>
2025-06-24 18:01:53 +0800

    fix

--- minference/modules/tri_mix.py
+++ minference/modules/tri_mix.py
@@ -60,7 +60,7 @@ def flexprefill_mix_forward(q, k, v, prefill_kwargs):
     global g
     layer_idx = prefill_kwargs["layer_idx"]
     starting_layer = prefill_kwargs["attn_forward_config"].get("starting_layer", 0)
-     if layer_idx == 0:
+    if layer_idx == 0:
         g["timer"] = [
             (torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True))
             for i in range(32)
