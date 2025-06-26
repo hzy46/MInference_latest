5b6b3fcf5803bd5ab76071ac1bca8ab3983c17dc
Zhiyuan He <zhiyuhe@microsoft.com>
2025-06-26 12:42:09 +0800

    fix

--- tri_mix/speed_test.py
+++ tri_mix/speed_test.py
@@ -130,7 +130,7 @@ def main(model_name="meta-llama/Llama-3.1-8B-Instruct", method="dense", starting
         # 80000,
         # 96000,
     ]
-    n_times = 3
+    n_times = 1
     if starting_layer is None:
         if model_name == "meta-llama/Llama-3.1-8B-Instruct":
             starting_layer = 16
