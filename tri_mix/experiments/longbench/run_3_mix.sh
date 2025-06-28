f5171cbb7a2e6509c1a51d27e3dca48b6fc44580
Zhiyuan He <zhiyuhe@microsoft.com>
2025-06-28 11:42:06 +0800

    fix

---
+++ tri_mix/experiments/longbench/run_3_mix.sh
@@ -0,0 +1,4 @@
+bash run_flexprefill_mix.sh llama3.1-8b-instruct
+bash run_flexprefill_mix.sh llama3-8b-instruct-262k
+bash run_flexprefill_mix.sh qwen2.5-7b-instruct
+cp -r pred_e/* $ZHIYUHE/new_longbench_skip_prefill_results
