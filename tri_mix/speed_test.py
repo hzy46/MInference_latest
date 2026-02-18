3057cd1ed6329608e4990de340aa12aefd27675d
Zhiyuan He <zhiyuhe@microsoft.com>
2026-02-18 21:39:58 +0900

    fix

--- tri_mix/speed_test.py
+++ tri_mix/speed_test.py
@@ -297,6 +297,7 @@ def main(
                 attn_implementation="flash_attention_2",
             )
 
+        print(model.config)
         model = minference_patch(model)
         samples = quick_get_random_kv_samples(
             model_name, tokenizer, 3000, n_kv_num=6000, n_sample=n_times
